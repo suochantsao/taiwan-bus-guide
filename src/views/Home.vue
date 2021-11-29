06463900d3acf9bd0ececeb0b2957a9453b4d282
suochantsao <suochantsao@gmail.com>
2021-11-29 16:03:00 +0800

    新增首頁 Background

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,9 @@
 <template>
-  <div class="home"></div>
+  <div class="home">
+    <h4>搜尋路線</h4>
+    <h4>搜尋站名</h4>
+    <h4>定位及地址搜尋</h4>
+  </div>
 </template>
 
 <script>
@@ -8,3 +12,9 @@ export default {
   components: {},
 };
 </script>
+
+<style lang="scss" scoped>
+.home {
+  background-image: url("../assets/img/bgImage.svg");
+}
+</style>
