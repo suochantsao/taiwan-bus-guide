7d4cbefaf4ba19db90718687fe0a3ab632498334
suochantsao <suochantsao@gmail.com>
2021-11-30 16:22:39 +0800

    新增 bus-section 和背景圖

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,10 @@
 <template>
   <div class="home">
-    <h4>搜尋路線</h4>
-    <h4>搜尋站名</h4>
-    <h4>定位及地址搜尋</h4>
+    <div class="bus-section">
+      <h4>搜尋路線</h4>
+      <h4>搜尋站名</h4>
+      <h4>定位及地址搜尋</h4>
+    </div>
   </div>
 </template>
 
@@ -15,6 +17,17 @@ export default {
 
 <style lang="scss" scoped>
 .home {
+  background-size: cover;
   background-image: url("../assets/img/bgImage.svg");
+  min-height: 100vh;
+  .bus-section {
+    margin: 0 auto;
+    background-image: url("../assets/img/bus.svg");
+    background-size: cover;
+    background-repeat: no-repeat;
+    height: 60%;
+    width: 80%;
+    background-position: top center;
+  }
 }
 </style>
