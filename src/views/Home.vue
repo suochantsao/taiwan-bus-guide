10f0d1ccb3a22a249b7ae75abf7eb1bd683828ff
suochantsao <suochantsao@gmail.com>
2021-12-02 23:46:06 +0800

    設定搜索標題

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,8 +2,8 @@
   <div class="home">
     <p>test</p>
     <div class="bus-section">
-      <h4>搜尋路線</h4>
-      <h4>搜尋站名</h4>
+      <h4 class="search-route">搜尋路線</h4>
+      <h4 class="search-station">搜尋站名</h4>
       <h4>定位及地址搜尋</h4>
     </div>
   </div>
@@ -26,13 +26,23 @@ export default {
     margin-bottom: 2%;
   }
   .bus-section {
+    display: flex;
     margin: 0 auto;
+    padding: 6% 0 0 30.5%;
     background-image: url("../assets/img/bus.svg");
     background-size: cover;
     background-repeat: no-repeat;
     height: 91vh;
     width: 89%;
     background-position: top;
+    font-weight: bold;
+    font-size: 30px;
+  }
+  .search-route {
+    margin-right: 18%;
+  }
+  .search-station {
+    margin-right: 12.5%;
   }
 }
 </style>
